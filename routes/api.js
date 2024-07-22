de8bab4ae5686fe06218efc7e367c11afb073854
Adamh1223 <adam@hussmedia.io>
2024-07-22 10:23:04 -0700

    added login/signup file, connected dashboard, blogpost, home

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,12 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const blogPostRoutes = require("./blogPostRoutes");
 const commentRoutes = require("./commentRoutes");
+const dashboardRoutes = require("../Controller/dashboardRoutes");
 
 // Define routes
 router.use("/users", userRoutes);
 router.use("/blogposts", blogPostRoutes);
 router.use("/comments", commentRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
 const router = require("express").Router();
+const { BlogPost, User, Comment } = require("../Models");
 
 // Import individual route handlers
 const apiRoutes = require("./api");
-const dashboardRoutes = require("../Controller/dashboardRoutes");
-const commentRoutes = require("./commentRoutes");
-const blogPostRoutes = require("./blogPostRoutes");
-const userRoutes = require("./userRoutes");
 
 // Prefix routes
 router.use("/api", apiRoutes);
-router.use("/dashboard", dashboardRoutes);
-router.use("/comments", commentRoutes);
-router.use("/blogposts", blogPostRoutes);
-router.use("/users", userRoutes);
+
+//put in homeroutes
+router.get("/", (req, res) => {
+  res.render("home");
+});
+router.get("/blogpost", (req, res) => {
+  res.render("blogPost");
+});
+router.get("/dashboard", async (req, res) => {
+  const blogPostData = await BlogPost.findAll({
+    include: [{ model: User, attributes: ["user_name"] }, { model: Comment }],
+  });
+  res.render("dashboard", {
+    blogPosts: blogPostData,
+  });
+});
+
+//do this for all handlebars pages^
 
 module.exports = router;
